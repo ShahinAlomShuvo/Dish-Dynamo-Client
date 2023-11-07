a30f6b6784b3d7c3c7467b9249a1fca230aef349
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-11-07 20:26:50 +0600

    add counter component

---
+++ src/Components/Counter/Counter.jsx
@@ -0,0 +1,38 @@
+import { FaUser, FaUtensilSpoon, FaUsers, FaAward } from "react-icons/fa";
+
+const Counter = () => {
+  return (
+    <div
+      className='bg-cover bg-center relative'
+      style={{
+        backgroundImage: "url(https://i.ibb.co/4jnZ7M6/counter-bg.jpg)",
+      }}
+    >
+      <div className='bg-black opacity-60 absolute inset-0'></div>
+      <div className='relative z-10 p-6 flex justify-evenly py-20 my-10'>
+        <div className='flex flex-col items-center space-y-5 text-white'>
+          <FaUser className='text-6xl p-4 bg-[#FF7C08] rounded-full' />
+          <h2 className='text-3xl font-bold'>75,000+</h2>
+          <p className='text-2xl font-bold'>Customer Service</p>
+        </div>
+        <div className='flex flex-col items-center space-y-5 text-white'>
+          <FaUtensilSpoon className='text-6xl p-4 bg-[#FF7C08] rounded-full' />
+          <h2 className='text-3xl font-bold'>100+</h2>
+          <p className='text-2xl font-bold'>Experience Chef</p>
+        </div>
+        <div className='flex flex-col items-center space-y-5 text-white'>
+          <FaUsers className='text-6xl p-4 bg-[#FF7C08] rounded-full' />
+          <h2 className='text-3xl font-bold'>50,000+</h2>
+          <p className='text-2xl font-bold'>Happy Customer</p>
+        </div>
+        <div className='flex flex-col items-center space-y-5 text-white'>
+          <FaAward className='text-6xl p-4 bg-[#FF7C08] rounded-full' />
+          <h2 className='text-3xl font-bold'>20+</h2>
+          <p className='text-2xl font-bold'>Winning Awards</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import HeroSlider from "../../Components/Header/Slider/HeroSlider";
 import TopFood from "../../Components/TopFood/TopFood";
+import Counter from "../../Components/Counter/Counter";
 
 const Home = () => {
   const foods = useLoaderData();
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <HeroSlider></HeroSlider>
       <TopFood foods={foods}></TopFood>
+      <Counter></Counter>
     </div>
   );
 };
