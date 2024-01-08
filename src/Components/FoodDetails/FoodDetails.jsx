4bb99600a692cf1dbafa90cd077dd08c55dfcc15
Shahin Alom <shahinalomshuvo94@gmail.com>
2024-01-08 19:17:35 +0600

    update foodDetails

--- src/Components/FoodDetails/FoodDetails.jsx
+++ src/Components/FoodDetails/FoodDetails.jsx
@@ -2,6 +2,8 @@ import Aos from "aos";
 import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { useLoaderData, Link } from "react-router-dom";
+import CommonBanner from "../CommonBanner/CommonBanner";
+import detailsBg from "../../assets/Images/detailsBg.jpg";
 
 const FoodDetails = () => {
   const food = useLoaderData();
@@ -25,8 +27,12 @@ const FoodDetails = () => {
       <Helmet>
         <title> DishDynamo | Details </title>
       </Helmet>
-      <div
-        className='flex justify-center'
+      <CommonBanner
+        bgImage={detailsBg}
+        title={"Food Description"}
+      ></CommonBanner>
+      {/* <div
+        className='flex justify-center my-10'
         style={{
           backgroundImage: `url(${foodImageUrl})`,
           backgroundSize: "cover",
@@ -67,6 +73,63 @@ const FoodDetails = () => {
             <Link
               to={`/orderingFood/${_id}`}
               className='btn bg-[#FF7C08] text-white border border-transparent hover:border-[#FF7C08] hover:bg-transparent hover:text-[#FF7C08] capitalize py-2 px-4 rounded-md transition duration-300 ease-in-out btn-block mt-2'
+            >
+              Order Now
+            </Link>
+          </div>
+        </div>
+      </div> */}
+      <div className='bg-[#FEF7EA]'>
+        <div className='grid grid-cols-2 gap-10 container mx-auto py-20 items-center'>
+          <img
+            data-aos='zoom-in'
+            className='rounded-3xl'
+            src={foodImageUrl}
+            alt=''
+          />
+          <div className='space-y-4'>
+            <p className='bg-gradient-to-r from-purple-700 to-purple-400 rounded-xl px-3 py-1 w-16 text-center'>
+              Sale!
+            </p>
+            <h2 className='text-3xl text-[#0c0c0c] font-semibold'>
+              {foodName}
+            </h2>
+            <p className='text-[#dc0505] text-xl'>Price: ${price}</p>
+            <hr />
+            <p className='text-2xl text-[#0c0c0c] font-semibold'>
+              Description:{" "}
+            </p>
+            <p className='text-[#666666] text-base font-normal'>
+              {description}
+            </p>
+            <hr />
+            <p className='text-2xl text-[#0c0c0c] font-semibold'>
+              Additional Info:
+            </p>
+            <div className='flex flex-wrap justify-between'>
+              <p className='text-[#dc0505]'>
+                {" "}
+                <span className='text-[#0c0c0c] mr-2'>Food Category :</span>
+                {foodCategory}
+              </p>
+              <p className='text-[#dc0505]'>
+                {" "}
+                <span className='text-[#0c0c0c] mr-2'>Food Origin :</span>
+                {foodOrigin}
+              </p>
+              <p className='text-[#dc0505]'>
+                {" "}
+                <span className='text-[#0c0c0c] mr-2'>Add By :</span>
+                {userName}
+              </p>
+            </div>
+            <hr />
+            <p className='text-2xl text-[#0c0c0c] font-semibold'>
+              Choose Your Options:
+            </p>
+            <Link
+              to={`/orderingFood/${_id}`}
+              className='btn bg-[#E6C9A2] text-[#0e1927] border border-transparent hover:border-[#E6C9A2] hover:bg-transparent  capitalize py-2 px-4 rounded-md transition duration-300 ease-in-out btn-block'
             >
               Order Now
             </Link>
